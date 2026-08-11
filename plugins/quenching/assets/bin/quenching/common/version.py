49f2245f11a2cc7e65ec8a48f2f048723424b531
Israel Holetz <israel.holetz@unicred.com.br>
2026-08-11 12:21:17 -0300

    release: 5.0.0 -> 6.0.0

--- plugins/quenching/assets/bin/quenching/common/version.py
+++ plugins/quenching/assets/bin/quenching/common/version.py
@@ -3,4 +3,4 @@
 Moved verbatim out of the pre-refactor specs script."""
 from __future__ import annotations
 
-VERSION = "5.0.0"  # kept in lockstep with the plugin VERSION file, plugin.json and marketplace.json
+VERSION = "6.0.0"  # kept in lockstep with the plugin VERSION file, plugin.json and marketplace.json
